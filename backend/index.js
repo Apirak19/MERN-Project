d4bfba8d933a9310d2fc9a49f909daaf3ca38d3d
Apirak <apirakfakin@gmail.com>
2023-09-27 16:05:59 +0700

    database created

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.get("/", (req, res) => {
-   res.send("app is working...")
-})
+const connectDB = async () => {
+   mongoose.connect('mongodb://localhost:27017/')
+}
 
 app.listen(5000, () => {
    console.log("server started on port 5000");
