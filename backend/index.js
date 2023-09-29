b6b328fdf2cc54056edd0468ba35116375ce66c4
Apirak <apirakfakin@gmail.com>
2023-09-29 16:21:48 +0700

    mongodb schema

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,31 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
 const connectDB = async () => {
-   mongoose.connect('mongodb://localhost:27017/')
+   try {  
+      mongoose.connect("mongodb://localhost:27017/MERN");
+      const productSchema = new mongoose.Schema({
+         name: String,
+         price: Number,
+      });
+      const product = mongoose.model("product", productSchema);
+      const data = await product.find();
+      console.warn(data);
+   }
+  catch (error) {
+console.error("Error:", error);
 }
+};
 
-app.listen(5000, () => {
-   console.log("server started on port 5000");
-})
+connectDB();
+
+app.listen(5000, (err) => {
+   if (err) {
+      console.error("There is an error");
+   }
+   else {
+      console.log("server started on port 5000");
+   }
+});
--- front-end/src/components/Signup.js
+++ front-end/src/components/Signup.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div>
-      <form className="m-auto flex flex-col justify-center items-center">
-        <h1>Register</h1>
-        <input type="text" />
-        <input type="text" />
+      <form className="flex flex-col justify-center items-center rounded-xl" onSubmit={(e) => {
+        e.preventDefault()
+      }}>
+        <h1 className="text-2xl font-bold mb-2">Register</h1>
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Enter your Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Enter your Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          className="input-box"
+          type="text"
+          placeholder="Enter your Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <button className="bg-slate-200 px-4 py-2 m-2 rounded-xl">
+          Sign up
+        </button>
       </form>
+      <h1>{name}</h1>
+      <h1>{email}</h1>
+      <h1>{password}</h1>
     </div>
   );
 };
