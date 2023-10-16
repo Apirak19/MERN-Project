b099569abf10b176924174f45c8610c63ff13697
Apirak <apirakfakin@gmail.com>
2023-10-16 14:59:14 +0700

    form added

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -1,15 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const AddProduct = () => {
   return (
     <div>
-        <h1>Add Product</h1>
-        <label htmlFor="productName" >Product Name</label><input type="text" id='productName' placeholder='Enter name of the product'/>
-        <input type="text" placeholder='Enter price of the product'/>
-        <input type="text" placeholder='Enter name of the product'/>
-        <input type="text" placeholder='Enter name of the product'/>
+      <h1>Add Product</h1>
+      <div>
+        <label htmlFor="productName">Product Name</label>
+        <input
+          type="text"
+          id="productName"
+          placeholder="Enter name of the product"
+        />
+      </div>
+      <div>
+        <label htmlFor="productName">Product Name</label>
+        <input
+          type="text"
+          id="productName"
+          placeholder="Enter name of the product"
+        />
+      </div>
+      <div>
+        <label htmlFor="productName">Product Name</label>
+        <input
+          type="text"
+          id="productName"
+          placeholder="Enter name of the product"
+        />
+      </div>
+      <div>
+        <label htmlFor="productName">Product Name</label>
+        <input
+          type="text"
+          id="productName"
+          placeholder="Enter name of the product"
+        />
+      </div>
+      <div>
+        <label htmlFor="productName">Product Name</label>
+        <input
+          type="text"
+          id="productName"
+          placeholder="Enter name of the product"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddProduct
+export default AddProduct;
