8e603c2e609f24036583e5e71996565d35e4bba1
Apirak <apirakfakin@gmail.com>
2023-10-17 13:54:19 +0700

    addProduct completed

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddProduct = () => {
-  console.warn();
+  const [pName, setPName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [category, setCateory] = useState("");
+  const [company, setCompany] = useState("");
+  const userID = JSON.parse(localStorage.getItem("user"))._id;
+  const navigate = useNavigate();
+  const addProduct = async () => {
+    if (pName && price && category && company) {
+      let result = await fetch("http://localhost:5000/add", {
+        method: "post",
+        body: JSON.stringify({ pName, price, category, userID, company }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      result = await result.json();
+      console.log(result);
+      alert("product added successfully");
+      navigate("/");
+    } else {
+      alert("Fill all required data");
+    }
+  };
   return (
     <div className="authPage ">
       <h1 className="title">Add Product</h1>
@@ -12,6 +35,10 @@ const AddProduct = () => {
             type="text"
             id="productName"
             placeholder="Enter name of the product"
+            value={pName}
+            onChange={(e) => {
+              setPName(e.target.value);
+            }}
           />
         </div>
         <div className="flex justify-between">
@@ -20,6 +47,10 @@ const AddProduct = () => {
             type="text"
             id="productPrice"
             placeholder="Enter price of the product"
+            value={price}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
           />
         </div>
         <div className="flex justify-between">
@@ -28,6 +59,10 @@ const AddProduct = () => {
             type="text"
             id="productCategory"
             placeholder="Enter category of the product"
+            value={category}
+            onChange={(e) => {
+              setCateory(e.target.value);
+            }}
           />
         </div>
         <div className="flex justify-between">
@@ -36,6 +71,8 @@ const AddProduct = () => {
             type="text"
             id="userID"
             placeholder="Enter userID"
+            value={userID}
+            onChange={() => {}}
           />
         </div>
         <div className="flex justify-between">
@@ -44,8 +81,15 @@ const AddProduct = () => {
             type="text"
             id="companyName"
             placeholder="Enter name of the company"
+            value={company}
+            onChange={(e) => {
+              setCompany(e.target.value);
+            }}
           />
         </div>
+        <button type="button" onClick={addProduct}>
+          Add
+        </button>
       </form>
     </div>
   );
