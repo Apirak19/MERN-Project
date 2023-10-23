df103d23453726fcc4a2787957d96623083c0bf2
Apirak Fakin <apirakfakin@gmail.com>
2023-10-23 21:22:52 +0700

    API added to button

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -13,12 +13,23 @@ const Products = () => {
     setProducts(result);
   };
   console.log(products);
+  const deleteProduct = async (id) => {
+    let result = await fetch(`http://localhost:5000/product/${id}`, {
+      method: "delete"
+    })
+    result = await result.json()
+    if (result) {
+      alert("record was deleted")
+      getProducts()
+    }
+  }
   return (
     <div className="page">
       <h1 className="title">Products List</h1>
       <div className="products-list">
         <ul>
           <li>No.</li>
+          <li>ID</li>
           <li>Name</li>
           <li>Price</li>
           <li>Category</li>
@@ -26,14 +37,23 @@ const Products = () => {
           <li>operation</li>
         </ul>
         {products.map((item, index) => {
-          return <ul key={item.name}>
-            <li>{index+1}</li>
-            <li>{item.name}</li>
-            <li>{item.price}</li>
-            <li>{item.category}</li>
-            <li>{item.company}</li>
-            <li><button className="productDel">delete</button></li>
-          </ul>;
+          return (
+            <ul key={item.name}>
+              <li>{index + 1}</li>
+              <li>{item._id}</li>
+              <li>{item.name}</li>
+              <li>{item.price}</li>
+              <li>{item.category}</li>
+              <li>{item.company}</li>
+              <li>
+                <button className="productDel" onClick={() => {
+                  deleteProduct(item._id)
+                }}>
+                  delete
+                </button>
+              </li>
+            </ul>
+          );
         })}
       </div>
     </div>
