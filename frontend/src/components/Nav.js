a17cc43fe31c8ed24519145bc4ea7a072e4e38f8
Apirak <apirakfakin@gmail.com>
2023-10-25 15:37:17 +0700

    buttonHidden

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Nav = () => {
-  const [screen, setScreen] = useState(window.innerWidth)
-  const [hidden, setHidden] = useState(true)
+  const [screen, setScreen] = useState(window.innerWidth);
+  const [buttonHidden, setButtonHidden] = useState(true);
+  const [navHidden, setNavHidden] = useState(true);
   const navigate = useNavigate();
   const auth = localStorage.getItem("user");
   const handleLogout = async () => {
@@ -12,21 +13,29 @@ const Nav = () => {
     navigate("/login");
   };
   window.addEventListener("resize", () => {
-    setScreen(window.innerWidth)
-  })
+    setScreen(window.innerWidth);
+  });
   useEffect(() => {
     if (auth) {
       navigate("/");
     }
   }, []);
   return (
-    hidden ? (<button onClick={() => setHidden(false)}>{screen}</button>)
-        : <div className={` ${hidden ? 'nav-hidden': 'nav'} shadow-xl`}>
-        <button className="w-3 absolute right-0 flex justify-center" onClick={()=>{setHidden(true)}}>X</button>
-        {auth ? (
-          <div className="nav-body flex justify-between">
+    <div className="navbar">
+      {auth && screen <= 821 && (
+        <>
+          <button
+            onClick={() => {
+              setHidden(false);
+            }}
+          >
+            Menu
+          </button>
+          <div
+            className={`nav-body ${hidden ? "hidden" : "flex justify-between"}`}
+          >
             <div className="nav-logo bg-blue-600 text-white rounded-full m-3 px-5 py-2">
-              <h1 className="logo font-bold ">LOGO</h1>
+              <h1 className="logo font-bold ">{screen}</h1>
             </div>
             <ul className="nav-ul flex justify-center">
               <li>
@@ -48,12 +57,41 @@ const Nav = () => {
               </li>
             </ul>
           </div>
-        ) : (
-          <div className="flex justify-between  ">
+        </>
+      )}
+      {auth && screen > 821 && (
+        <div className="nav-body flex justify-between ">
+          <div className="nav-logo bg-blue-600 text-white rounded-full m-3 px-5 py-2">
+            <h1 className="logo font-bold ">{screen}</h1>
+          </div>
+          <ul className="nav-ul flex justify-center">
+            <li>
+              <Link to={"/"}>Product</Link>
+            </li>
+            <li>
+              <Link to={"/add"}>Add Product</Link>
+            </li>
+            <li>
+              <Link to={"/edit"}>Edit Product</Link>
+            </li>
+            <li>
+              <Link to={"/profile"}>Profile</Link>
+            </li>
+            <li>
+              <Link to={"/login"} onClick={handleLogout}>
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+      {!auth && screen <= 821 && (
+        <>
+          <div className={`nav-body  ${hidden ? "hidden" : "flex justify-between"}`}>
             <div className="bg-blue-600 text-white rounded-full m-3 px-5 py-2 transform transition-all duration-100 ease-in hover:scale-110">
-              <h1 className="logo font-bold ">LOGO</h1>
+              <h1 className="logo font-bold ">{screen}</h1>
             </div>
-            <ul className="nav-ul flex justify-end ">
+            <ul className="nav-ul flex justify-end">
               <li>
                 <Link to={"/login"}>Login</Link>
               </li>
@@ -62,9 +100,24 @@ const Nav = () => {
               </li>
             </ul>
           </div>
-        )}
-</div>
-    
+        </>
+      )}
+      {!auth && screen > 821 && (
+        <div className="flex justify-between">
+          <div className="bg-blue-600 text-white rounded-full m-3 px-5 py-2 transform transition-all duration-100 ease-in hover:scale-110">
+            <h1 className="logo font-bold ">{screen}</h1>
+          </div>
+          <ul className="nav-ul flex justify-end">
+            <li>
+              <Link to={"/login"}>Login</Link>
+            </li>
+            <li>
+              <Link to={"/register"}>Signup</Link>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 };
 
