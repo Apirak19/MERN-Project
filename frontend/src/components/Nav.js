8024c55a15bebf8f96ac2417621644c1aeb4b76e
Apirak Fakin <apirakfakin@gmail.com>
2023-10-29 21:01:46 +0700

    nav mui added

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,10 +3,9 @@ import { Link, useNavigate } from "react-router-dom";
 import Products from "./Products";
 import AddProduct from "./AddProduct";
 import PrivateComponent from "./PrivateComponent";
-import 
+import { Button, Grid, Typography } from "@mui/material";
 
 const Nav = () => {
-  const [screen, setScreen] = useState(window.innerWidth);
   const [buttonHidden, setButtonHidden] = useState(true);
   const clearState = () => setNavHidden(true);
   const [navHidden, setNavHidden] = useState(true);
@@ -17,79 +16,111 @@ const Nav = () => {
     alert("Log out successfully");
     navigate("/login");
   };
-  window.addEventListener("resize", () => {
-    setScreen(window.innerWidth);
-  });
+
   useEffect(() => {
     if (auth) {
       navigate("/");
     }
   }, []);
   return (
-    <nav className={`${auth ? "nav" : "hidden"}`}>
-      <div className="logo-btn">
-        <span className="logo">o</span>
-      </div>
-      <div className="menu-btn h-20 inline-block fixed right-0">
-        <span className="menu" onClick={(e) => setNavHidden(!navHidden)}>
-          Menu
-        </span>
-      </div>
-      {auth ? (
-        <ul className={`navbar ${navHidden ? "navbar-hide" : ""}`}>
-          <li>
-            <Link to="/" onClick={clearState}>
-              <div className="navlist">
-                <div className="icon">o</div>
-                <p className="flex justify-center items-center">Products</p>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/add" onClick={clearState}>
-              <div className="navlist">
-                <div className="icon">o</div>
-                <p className="flex justify-center items-center">Add Product</p>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/edit" onClick={clearState}>
-              <div className="navlist">
-                <div className="icon">o</div>
-                <p className="flex justify-center items-center">Edit Product</p>
-              </div>
-            </Link>
-          </li>
-          <li onClick={handleLogout} className="cursor-pointer">
+    // <nav className={`${auth ? "nav" : "hidden"}`}>
+    //   <div className="logo-btn">
+    //     <span className="logo">o</span>
+    //   </div>
+    //   <div className="menu-btn h-20 inline-block fixed right-0">
+    //     <span className="menu" onClick={(e) => setNavHidden(!navHidden)}>
+    //       Menu
+    //     </span>
+    //   </div>
+    //   {auth ? (
+    //     <ul className={`navbar ${navHidden ? "navbar-hide" : ""}`}>
+    //       <li>
+    //         <Link to="/" onClick={clearState}>
+    //           <div className="navlist">
+    //             <div className="icon">o</div>
+    //             <p className="flex justify-center items-center">Products</p>
+    //           </div>
+    //         </Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/add" onClick={clearState}>
+    //           <div className="navlist">
+    //             <div className="icon">o</div>
+    //             <p className="flex justify-center items-center">Add Product</p>
+    //           </div>
+    //         </Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/edit" onClick={clearState}>
+    //           <div className="navlist">
+    //             <div className="icon">o</div>
+    //             <p className="flex justify-center items-center">Edit Product</p>
+    //           </div>
+    //         </Link>
+    //       </li>
+    //       <li onClick={handleLogout} className="cursor-pointer">
+    //         <div className="navlist">
+    //           <div className="icon">o</div>
+    //           <p className="flex justify-center items-center">Logout</p>
+    //         </div>
+    //       </li>
+    //     </ul>
+    //   ) : (
+    //     <ul className="">
+    //       <li>
+    //         <Link to="/login">
+    //           <div className="navlist">
+    //             <div className="icon">o</div>
+    //             <p className="flex justify-center items-center">Login</p>
+    //           </div>
+    //         </Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/register">
+    //           <div className="navlist">
+    //             <div className="icon">o</div>
+    //             <p className="flex justify-center items-center">Sign up</p>
+    //           </div>
+    //         </Link>
+    //         </li>
+    //         <p>test</p>
+    //     </ul>
+    //   )}
+    // </nav>
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={1}
+    >
+      <Grid item xs={6} md={8}>
+        <Typography variant="h1">Logo</Typography>
+      </Grid>
+      <Grid>
+        <Button variant="contained">
+          <Link to="/login">
             <div className="navlist">
-              <div className="icon">o</div>
-              <p className="flex justify-center items-center">Logout</p>
+              <p className="flex justify-center items-center">Login</p>
             </div>
-          </li>
-        </ul>
-      ) : (
-        <ul className="">
-          <li>
-            <Link to="/login">
-              <div className="navlist">
-                <div className="icon">o</div>
-                <p className="flex justify-center items-center">Login</p>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/register">
-              <div className="navlist">
-                <div className="icon">o</div>
-                <p className="flex justify-center items-center">Sign up</p>
-              </div>
-            </Link>
-            </li>
-            <p>test</p>
-        </ul>
-      )}
-    </nav>
+          </Link>
+        </Button>
+        <Button variant="outlined">
+          <Link to="/register">
+            <div className="navlist">
+              <p className="flex justify-center items-center">Register</p>
+            </div>
+          </Link>
+        </Button>
+        <Button variant="outlined">
+          <Link to="/register">
+            <div className="navlist">
+              <p className="flex justify-center items-center">Register</p>
+            </div>
+          </Link>
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
